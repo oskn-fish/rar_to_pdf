f26cfe973741625a6f922aab897fcf7b3be652fa
Shunnoske Chinen <flash_wave@icloud.com>
2023-07-29 13:01:30 +0900

    removed unnecessary logs

--- recursive_img2pdf.py
+++ recursive_img2pdf.py
@@ -13,19 +13,19 @@ logger.addHandler(handler)
 
 def recursive_img2pdf(path):
     images_path = glob.glob(glob.escape(path)+"/*.jpg")+glob.glob(glob.escape(path)+"/*.png")
-    images_path.sort()
-    if images_path!=[]:
-        logger.debug(path)
+    
+    if images_path:
+        images_path.sort()
         skip = False
-
+        
         if os.path.isfile(path+".pdf"):
-            # print(f"'{path}.pdf' already exists.")
             ans = ''
             while ans not in ['y', 'n']:
                 ans = input(f"'{path}.pdf' already exists. Do you want to overwrite? (y[Yes]/n[No])")
                 ans = ans.lower()
             if ans == 'n':
                 skip = True
+                
         if not skip:
             logger.debug(f"saving to {path}.pdf ...")
             with open(path+".pdf", mode='wb') as f:
@@ -34,7 +34,6 @@ def recursive_img2pdf(path):
     for child in os.listdir(path):
         child_path = path+'/'+child
         if os.path.isdir(child_path):
-            logger.debug(f"moving to {child_path} ...")
             recursive_img2pdf(child_path)
 
 def get_args():
